08d4bfa228cca0fd250a9a3ca3c69e584f097995
Tamara Patterson <98510821+tamarabuilds@users.noreply.github.com>
2023-11-02 14:54:00 -0700

    initial rest api with express files

---
+++ rest_apis_express_projectfiles/S2V4_refactor_getone/app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+
+const records = require('./records');
+
+// Send a GET request to /quotes to READ a list of quotes
+app.get('/quotes', async (req, res)=>{
+    const quotes = await records.getQuotes();
+    res.json(quotes);
+});
+// Send a GET request to /quotes/:id to READ(view) a quote
+app.get('/quotes/:id', (req, res)=>{
+
+});
+// Send a POST request to /quotes to  CREATE a new quote 
+// Send a PUT request to /quotes/:id to UPDATE (edit) a quote
+// Send a DELETE request to /quotes/:id DELETE a quote 
+// Send a GET request to /quotes/quote/random to READ (view) a random quote
+
+app.listen(3000, () => console.log('Quote API listening on port 3000!'));
+
---
+++ rest_apis_express_projectfiles/S3V4_http_status_codes/app.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const app = express();
+
+const records = require('./records');
+
+app.use(express.json());
+
+// Send a GET request to /quotes to READ a list of quotes
+app.get('/quotes', async (req, res)=>{
+    const quotes = await records.getQuotes();
+    res.json(quotes);
+});
+// Send a GET request to /quotes/:id to READ(view) a quote
+app.get('/quotes/:id', async (req, res)=>{
+    try {
+        const quote = await records.getQuote(req.params.id);
+        res.json(quote);
+    } catch(err) {
+        res.json({message: err.message});
+    }
+});
+
+//Send a POST request to /quotes to  CREATE a new quote 
+app.post('/quotes', async (req,res) =>{
+    try {
+        const quote = await records.createQuote({
+            quote: req.body.quote,
+            author: req.body.author
+        });
+        res.json(quote);
+    } catch(err) {
+        res.json({message: err.message});
+    } 
+});
+// Send a PUT request to /quotes/:id to UPDATE (edit) a quote
+// Send a DELETE request to /quotes/:id DELETE a quote 
+// Send a GET request to /quotes/quote/random to READ (view) a random quote
+
+app.listen(3000, () => console.log('Quote API listening on port 3000!'));
+
