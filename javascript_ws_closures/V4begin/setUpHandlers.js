40180bdb35df2017d15eed01d67feb4fe65111cb
Tamara Patterson <98510821+tamarabuilds@users.noreply.github.com>
2023-11-03 08:58:11 -0700

    completed lessons on closures

--- javascript_ws_closures/V4begin/setUpHandlers.js
+++ javascript_ws_closures/V4begin/setUpHandlers.js
@@ -1,9 +1,15 @@
 var buttons = document.getElementsByTagName('button');
 
+function createHandler(name){
+	return function(){
+		console.log(name);
+	}
+}
+
+
 for(var i = 0; i < buttons.length; i += 1) {
 	var button = buttons[i];
 	var buttonName = button.innerHTML;
-	button.addEventListener('click', function() {
-		console.log(buttonName);
-	});
-}
+	button.addEventListener('click', createHandler(buttonName))
+};
+
